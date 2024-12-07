b37fd2e50a13f0f7fc69957a7e0c090df5891be7
rizqya-adzra <rizqyaadzrazahirasudrajat@smkwikrama.sch.id>
2024-12-07 08:09:33 +0700

    secondCommit

--- src/components/ContactItemBody.jsx
+++ src/components/ContactItemBody.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function ContactItemBody({ name, tag }) {
     return (
-        <div className="contact.item_body">
-            <h3 className="contact.item_title">{name}</h3>
-            <p className="contact.item_username">{tag}</p>
+        <div className="contact-item__body">
+            <h3 className="contact-item__title">{name}</h3>
+            <p className="contact-item__username">{tag}</p>
         </div>
     )
 } 
